fe0d0e6df663058e8eb8f13a8841b6c9ae8c6d76
tauheed80 <tauheed80@users.noreply.github.com>
2026-06-18 01:40:14 +0000

    Tetris: Add StatixOS device makefile (android-16/vic)
    - statix_Tetris.mk: StatixOS config with maintainer zulhvoid
    - Inherits common StatixOS config (auto GApps), device.mk, vendor
    - AndroidProducts.mk: Added statix_Tetris lunch variants

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,9 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/infinity_Tetris.mk
+    $(LOCAL_DIR)/infinity_Tetris.mk \
+    $(LOCAL_DIR)/statix_Tetris.mk
+
+COMMON_LUNCH_CHOICES := \
+    infinity_Tetris-userdebug \
+    infinity_Tetris-eng \
+    statix_Tetris-userdebug \
+    statix_Tetris-eng
